1c4c1102f666c40478942827be0f40553a7db239
akacronos <jorge.escamilla89@gmail.com>
2015-11-24 16:48:59 -0600

    Add npm modules to project

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    postcss = require('gulp-postcss'),
+    uglify = require('gulp-uglify'),
+    stylus = require('gulp-stylus'),
+    livereload = require('gulp-livereload'),
+    autoprefixer = require('autoprefixer'),
+    rupture = require('rupture'),
+    lost = require('lost'),
+    rucksack = require('rucksack-css');
+
+function errorLog(error){
+  console.error.bind(error);
+  this.emit('end')
+}
+
+
+// Connect Task
+// Uglifies javascript
+gulp.task('connect', function(){
+  connect.server({
+    livereload: true,
+    port: 5000
+  });
+});
+
+// Html Task
+// Reloads html files
+gulp.task('html', function (){
+  gulp.src('./*.html')
+      .pipe(livereload());
+});
+
+
+// Scripts Task
+// Uglifies javascript
+gulp.task('scripts', function(){
+  gulp.src('js/*.js')
+      .pipe(uglify())
+      .on('error', errorLog)
+      .pipe(gulp.dest('minjs'));
+});
+
+// Styles Task
+// Compile and minify stylus 
+gulp.task('styles', function(){
+  var processors = [
+    rucksack,
+    lost,
+    autoprefixer ({browsers:['last 2 version']})
+  ];
+
+  gulp.src('css/styles.styl')
+      .pipe(stylus({
+        compress: false,
+        use: [rupture()]
+      }))
+      .pipe(postcss(processors))
+      .pipe(gulp.dest('css/'))
+      .pipe(livereload());
+});
+
+// Watch Task
+// Watches files
+gulp.task('watch', function(){
+  livereload.listen();
+
+  gulp.watch('./**/*.html', ['html']);
+  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('css/**/*.styl', ['styles']);
+});
+
+gulp.task('default', ['connect', 'scripts', 'styles', 'watch']);
