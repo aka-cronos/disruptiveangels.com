acdf5835bd8e9a3019c20371b05f996da7d6cf74
Jorge Escamilla <jorge.escamilla89@gmail.com>
2017-08-10 21:49:10 -0500

    Add contact form

--- js/main.js
+++ js/main.js
@@ -487,3 +487,19 @@ $(document).ready(function () {
     }
   });
 });
+function sendEmail(){
+  message = "Name: " + $("#name").val() + "Email: " + $("#mail").val() + "Message: " + $("#message").val();
+
+  $.post("https://formspree.io/cronos@disruptiveangels.com", {message: message})
+  .done(function() {
+    $( ".contact" ).empty();
+    $( ".contact" ).append( '<h2 class="title">Your message was send succesfully.</h2>' );
+    setTimeout(function() {
+      $('.featherlight').click();
+    }, 1000);
+  })
+  .fail(function() {
+    $( ".form-message" ).append( 'UPS! Try again.' );
+  });
+  return false;
+}
